e9455196a6ecfcca6a2096508ed74e4b8f5644d8
Adam Shell <adamstcn@gmail.com>
2011-03-23 22:10:23 -0400

    Add Camera class

---
+++ src/rogueshadow/XionEngine/Camera.java
@@ -0,0 +1,35 @@
+package rogueshadow.XionEngine;
+
+
+public class Camera {
+	int x;
+	int y;
+	int zoom = 100;
+	public Camera(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	public int[] moveCam(int deltax, int deltay){
+		this.x += deltax;
+		this.y += deltay;
+		return new int[] {this.x,this.y};
+	}
+	public int getScreenX(int worldX){
+		return (worldX + x*zoom);
+	}
+	public int getScreenY(int worldY){
+		return (worldY + y*zoom);
+	}
+	public int getWorldX(int screenX){
+		return (screenX - x*zoom);
+	}
+	public int getWorldY(int screenY){
+		return (screenY - y*zoom);
+	}
+	public int getZoom(){
+		return zoom;
+	}
+	public void setZoom(int zoom){
+		this.zoom = zoom;
+	}
+}
